1b62a0fc5f5186a714e54bbb9396f85c87ca9a09
Raymond Seger <raymondseger@live.com>
2017-04-29 10:56:55 +0700

    button with CSS

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -42,34 +42,10 @@ public class Main extends Application {
         StackPane root = new StackPane();
         Scene scene = new Scene(root, 300, 250);
 
-        //defining the axes
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Number of Month");
-        //creating the chart
-        final LineChart<Number,Number> lineChart =
-                new LineChart<Number,Number>(xAxis,yAxis);
+        Button buttonColor = new Button("Color");
+        buttonColor.setStyle("-fx-background-color: slateblue; -fx-text-fill: white;");
 
-        lineChart.setTitle("Stock Monitoring, 2010");
-        //defining a series
-        XYChart.Series series = new XYChart.Series();
-        series.setName("My portfolio");
-        //populating the series with data
-        series.getData().add(new XYChart.Data(1, 23));
-        series.getData().add(new XYChart.Data(2, 14));
-        series.getData().add(new XYChart.Data(3, 15));
-        series.getData().add(new XYChart.Data(4, 24));
-        series.getData().add(new XYChart.Data(5, 34));
-        series.getData().add(new XYChart.Data(6, 36));
-        series.getData().add(new XYChart.Data(7, 22));
-        series.getData().add(new XYChart.Data(8, 45));
-        series.getData().add(new XYChart.Data(9, 43));
-        series.getData().add(new XYChart.Data(10, 17));
-        series.getData().add(new XYChart.Data(11, 29));
-        series.getData().add(new XYChart.Data(12, 25));
-        lineChart.getData().add(series);
-
-        root.getChildren().add(lineChart);
+        root.getChildren().add(buttonColor);
 
         primaryStage.setScene(scene);
         primaryStage.show();
