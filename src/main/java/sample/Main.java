25612c3a78ffc171168117f133aa4ac6383c8bfe
Raymond Seger <raymondseger@live.com>
2017-04-29 09:29:08 +0700

    choicebox

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,15 +2,13 @@ package sample;
 
 import com.sun.javafx.css.Stylesheet;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -36,24 +34,14 @@ public class Main extends Application {
         StackPane root = new StackPane();
         Scene scene = new Scene(root, 300, 250);
 
-        Label label = new Label ("Values");
-        label.setFont(new Font("Cambria", 32));
-        label.setRotate(270);
-        label.setTranslateY(50);
+        ChoiceBox cb = new ChoiceBox();
+        cb.setItems(FXCollections.observableArrayList(
+                "New Document", "Open ",
+                new Separator(), "Save", "Save as")
+        );
+        cb.setTooltip(new Tooltip("Select one"));
 
-        Label label3 = new Label("A label that needs to be wrapped");
-        label3.setWrapText(true);
-        label3.setOnMouseEntered((MouseEvent e) -> {
-            label3.setScaleX(1.5);
-            label3.setScaleY(1.5);
-        });
-        label3.setOnMouseExited((MouseEvent e) -> {
-            label3.setScaleX(1);
-            label3.setScaleY(1);
-        });
-
-        root.getChildren().add(label);
-        root.getChildren().add(label3);
+        root.getChildren().add(cb);
 
         primaryStage.setScene(scene);
         primaryStage.show();
