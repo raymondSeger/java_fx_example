11853cce2fcb133838451bcdc462d17c7ff463f2
Raymond Seger <raymondseger@live.com>
2017-04-29 09:00:36 +0700

    example of creating ID and creating the CSS attributes.
    But adding the stylesheet not yet

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,12 +1,11 @@
 package sample;
 
+import com.sun.javafx.css.Stylesheet;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,8 +13,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -40,7 +37,10 @@ public class Main extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
+        Scene scene = new Scene(grid, 300, 275);
+
         Text scenetitle = new Text("Welcome");
+        scenetitle.setId("welcome-text");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
 
@@ -68,7 +68,6 @@ public class Main extends Application {
             }
         });
 
-        Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
 
         primaryStage.show();
